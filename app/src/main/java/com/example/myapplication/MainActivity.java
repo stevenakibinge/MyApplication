01da59d31c17f2d8615c3d57230dabb948c95f87
Steve <sbiggzgeo@gmail.com>
2020-02-15 19:38:42 +0300

    AI application

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view){
+        EditText message=(EditText)findViewById(R.id.message);
+        Intent intent=new Intent(this,DisplayMessageActivity.class);
+        intent.putExtra("MESSAGE",message.getText().toString());
+        startActivity(intent);
+        message.setText("");
+    }
+}
