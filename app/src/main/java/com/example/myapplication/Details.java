cc026492d87d19014f8499914b27c1b389376b71
stevenakibinge <sbiggzgeo@gmail.com>
2020-12-17 18:46:31 +0300

    saving files to both internal and external storage and retrieving

--- app/src/main/java/com/example/myapplication/Details.java
+++ app/src/main/java/com/example/myapplication/Details.java
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Details extends AppCompatActivity {
     FileInputStream fstream;
@@ -15,5 +22,31 @@ public class Details extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        TextView result = (TextView)findViewById(R.id.resultView);
+        Button back = (Button)findViewById(R.id.btnBack);
+        try {
+            File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+            File myFile = new File(folder,"user_details");
+            fstream = new FileInputStream(myFile);
+            StringBuffer sbuffer = new StringBuffer();
+            int i;
+            while ((i = fstream.read())!= -1){
+                sbuffer.append((char)i);
+            }
+            fstream.close();
+            String details[] = sbuffer.toString().split("\n");
+            result.setText("Name: "+ details[0]+"\nPassword: "+details[1]);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(Details.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-}
+    }
--- app/src/main/java/com/example/myapplication/ExternalStorage.java
+++ app/src/main/java/com/example/myapplication/ExternalStorage.java
@@ -1,79 +1,56 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class ExternalStorage extends AppCompatActivity {
-    EditText editTextFileName,editTextData;
-    Button saveButton,readButton;
+    EditText uname, pwd;
+    Button saveBtn;
+    FileOutputStream fstream;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_external_storage);
-        editTextFileName=findViewById(R.id.editText1);
-        editTextData=findViewById(R.id.editText2);
-        saveButton=findViewById(R.id.button1);
-        readButton=findViewById(R.id.button2);
-        saveButton.setOnClickListener(new View.OnClickListener(){
-
+        uname = (EditText)findViewById(R.id.txtName);
+        pwd = (EditText)findViewById(R.id.txtPwd);
+        saveBtn = (Button)findViewById(R.id.btnSave);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View arg0) {
-                String filename=editTextFileName.getText().toString();
-                String data=editTextData.getText().toString();
-
-                FileOutputStream fos;
+            public void onClick(View v) {
+                String username = uname.getText().toString()+"\n";
+                String password = pwd.getText().toString();
                 try {
-                    File myFile = new File("/sdcard/"+filename);
-                    myFile.createNewFile();
-                    FileOutputStream fOut = new FileOutputStream(myFile);
-                    OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
-                    myOutWriter.append(data);
-                    myOutWriter.close();
-                    fOut.close();
-                    Toast.makeText(getApplicationContext(),filename + "saved",Toast.LENGTH_LONG).show();
-                } catch (FileNotFoundException e) {e.printStackTrace();}
-                catch (IOException e) {e.printStackTrace();}
-            }
-        });
-
-        //Performing action on Read Button
-        readButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View arg0) {
-                String filename=editTextFileName.getText().toString();
-                StringBuffer stringBuffer = new StringBuffer();
-                String aDataRow = "";
-                String aBuffer = "";
-                try {
-                    File myFile = new File("/sdcard/"+filename);
-                    FileInputStream fIn = new FileInputStream(myFile);
-                    BufferedReader myReader = new BufferedReader(
-                            new InputStreamReader(fIn));
-                    while ((aDataRow = myReader.readLine()) != null) {
-                        aBuffer += aDataRow + "\n";
-                    }
-                    myReader.close();
+                    ActivityCompat.requestPermissions(ExternalStorage.this, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},23);
+                    File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+                    File myFile = new File(folder,"user_details");
+                    fstream = new FileOutputStream(myFile);
+                    fstream.write(username.getBytes());
+                    fstream.write(password.getBytes());
+                    fstream.close();
+                    Toast.makeText(getApplicationContext(), "Details Saved in "+myFile.getAbsolutePath(),Toast.LENGTH_SHORT).show();
+                    intent = new Intent(ExternalStorage.this,Details.class);
+                    startActivity(intent);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                Toast.makeText(getApplicationContext(),aBuffer,Toast.LENGTH_LONG).show();
             }
         });
     }
-
 }
